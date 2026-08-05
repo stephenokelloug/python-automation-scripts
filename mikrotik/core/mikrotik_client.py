d76c81bcf582f776adcb7caabcce510d1ff030c1
Stephen Okello <okellostephenomoding@gmail.com>
2026-08-05 11:19:13 +0300

    v0.4.2

--- mikrotik/core/mikrotik_client.py
+++ mikrotik/core/mikrotik_client.py
@@ -14,7 +14,17 @@ class MikroTikClient:
         try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(self.host, username=self.user, password=self.password)
+            #ssh.connect(self.host, username=self.user, password=self.password)
+            ssh.connect(
+                self.host,
+                port=22,
+                username=self.user,
+                password=self.password,
+                look_for_keys=False,
+                allow_agent=False,
+                timeout=10
+            )
+
             stdin, stdout, stderr = ssh.exec_command(cmd)
             output = stdout.read().decode().strip() or stderr.read().decode().strip()
             ssh.close()
